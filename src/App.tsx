8fa4b4e168a39a9c44d8b147f5cc974a4dc1dc16
DandunAdi <dandun.adi@gmail.com>
2023-10-31 13:49:40 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <section className="h-screen flex justify-center items-center bg-slate-500 ">
+        <h2 className="p-5 bg-rose-950 text-8xl text-white font-bold">A</h2>
+      </section>
+      <section className="h-screen flex justify-center items-center bg-green-900 ">
+        <h2 className="p-5 bg-rose-950 text-8xl text-white font-bold">B</h2>
+      </section>
+      <section className="h-screen flex justify-center items-center bg-yellow-700 ">
+        <h2 className="p-5 bg-rose-950 text-8xl text-white font-bold">C</h2>
+      </section>
+      <section className="h-screen flex justify-center items-center bg-purple-800 ">
+        <h2 className="p-5 bg-rose-950 text-8xl text-white font-bold">D</h2>
+      </section>
+    </>
+  );
+}
+
+export default App;
